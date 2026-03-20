adca3e9d3883f6214c4ad088c8dfc2ef135bcc1a
szonjauley <szul@itu.dk>
2026-03-20 11:50:10 +0100

    feat(normality): adding shapiro_test function

--- AUs/au_normality.py
+++ AUs/au_normality.py
@@ -1,7 +1,20 @@
 import pandas as pd
 from pathlib import Path
+from scipy.stats import shapiro
 
 
 def load_data(file: Path) -> pd.DataFrame:
     """Load AU data"""
     return pd.read_csv(file)
+
+def shapiro_test(x) -> pd.Series:
+    """
+    Run Shapiro normality test
+    """
+    stat, p = shapiro(x)
+    return pd.Series({
+        "n": len(x),
+        "shapiro_stat": stat,
+        "p_value": p,
+        "normal": p > (0.05/14) #dividing by 14 because of multiple comparison amongst the 14 AU values
+    })
