0b6b6f14bad1f4265e3a7c0d0ac23dcbec5fcadc
szonjauley <szul@itu.dk>
2026-03-24 11:42:18 +0100

    chore(gaze_aggregation): updating paths and column names

--- gaze/gaze_aggregation.py
+++ gaze/gaze_aggregation.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from pathlib import Path
-import argparse
 
+# Directory where the current script is located
+SCRIPT_DIR = Path(__file__).parent.resolve() # BachelorProject/gaze
+DATA_DIR = SCRIPT_DIR.parent / "data" # BachelorProject/data
 
 def load_data(file: Path) -> pd.DataFrame:
     """
@@ -14,31 +16,31 @@ def aggregate_file(df: pd.DataFrame, segment: str) -> pd.DataFrame:
     Aggregate values for a single file and format output
     """
     # get depression label per person
-    depression_map = df.groupby("person_ID")["depression"].first()
+    depression_map = df.groupby("person_id")["depressed"].first()
 
     # compute stats
     stats = (
-        df.groupby("person_ID")["delta_deg"]
+        df.groupby("person_id")["delta_deg"]
         .agg(["mean", "std"])
         .reset_index()
     )
 
     # convert to long format
     stats = stats.melt(
-        id_vars="person_ID",
+        id_vars="person_id",
         value_vars=["mean", "std"],
         var_name="stat",
-        value_name="delta_deg"
+        value_name="value"
     )
 
     # attach depression label
-    stats["depression"] = stats["person_ID"].map(depression_map)
+    stats["depressed"] = stats["person_id"].map(depression_map)
 
     # add segment label
-    stats["segment"] = segment
+    stats["segment_type"] = segment
 
     # reorder columns
-    stats = stats[["person_ID", "stat", "depression", "segment", "delta_deg"]]
+    stats = stats[["person_id", "stat", "depressed", "segment_type", "value"]]
 
     return stats
 
@@ -54,42 +56,26 @@ def combine_files(files:list, segments:list) -> pd.DataFrame:
     combined = pd.concat(dfs, ignore_index=True)
 
     # order by person_ID
-    combined = combined.sort_values(["person_ID", "segment", "stat"])
+    combined = combined.sort_values(["person_id", "segment_type", "stat"])
 
     return combined
 
-def main(base_folder: Path):
+def main():
 
-    base_path = Path(base_folder)
     files = [
-        base_path / "combined_gaze_deltas_all.csv",
-        base_path / "listening_gaze_deltas_all.csv",
-        base_path / "speaking_gaze_deltas_all.csv"
+        DATA_DIR / "combined_gaze_deltas.csv",
+        DATA_DIR / "listening_gaze_deltas.csv",
+        DATA_DIR / "speaking_gaze_deltas.csv"
     ]
 
     segments = ["all", "listening", "speaking"]
 
     combined = combine_files(files, segments)
 
-    combined.to_csv("gaze_aggregation.csv", index=False)
+    output_path = DATA_DIR / "gaze_aggregation.csv"
+    combined.to_csv(output_path, index=False)
 
     return
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Combine and aggregate gaze values"
-    )
-
-    parser.add_argument(
-        "--folder",
-        type=str,
-        required=True,
-        help="Path to base folder containing delta files"
-    )
-
-    args = parser.parse_args()
-    main(
-        base_folder=args.folder
-    )
-
-### example run: python gaze_aggregation.py --folder /Users/szonja/Documents/ITU/Thesis
+    main()
